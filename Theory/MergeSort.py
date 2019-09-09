fe26affca50c1e52976e67c52103431b34512a3e
ag502 <ag502@naver.com>
2019-09-09 10:55:09 +0900

    Implement MergeSort

--- Theory/MergeSort.py
+++ Theory/MergeSort.py
@@ -18,9 +18,13 @@ def combine(list1, list2):
     return new_list
 
 
-print(combine([1], []))
-print(combine([], [1]))
-print(combine([2], [1]))
-print(combine([1, 2, 3, 4], [5, 6, 7, 8]))
-print(combine([5, 6, 7, 8], [1, 2, 3, 4]))
-print(combine([4, 7, 8, 9], [1, 3, 6, 10]))
+def merge_sort(input_list):
+    if len(input_list) <= 1:
+        return input_list
+    return combine(merge_sort(input_list[0:len(input_list) // 2]),
+                   merge_sort(input_list[len(input_list) // 2:]))
+
+
+print(merge_sort([1, 3, 5, 7, 9, 11, 13, 11]))
+print(merge_sort([28, 13, 9, 30, 1, 48, 5, 7, 15]))
+print(merge_sort([2, 5, 6, 7, 1, 2, 4, 7, 10, 11, 4, 15, 13, 1, 6, 4]))
