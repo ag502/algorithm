9195e074d7704164c8c3a2f064087068ab79113a
ag502 <ag502@naver.com>
2019-10-01 01:41:08 +0900

    Solve the Problem

--- Problem/MockTest/src/Solution.java
+++ Problem/MockTest/src/Solution.java
@@ -1,11 +1,11 @@
 import java.util.*;
 
 public class Solution {
-    private final int[] STUDENT_A = {1, 2, 3, 4, 5};
-    private final int[] STUDENT_B = {2, 1, 2, 3, 2, 4, 2, 5};
-    private final int[] STUDENT_C = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    private final int[] STUDENT_A = { 1, 2, 3, 4, 5 };
+    private final int[] STUDENT_B = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    private final int[] STUDENT_C = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
 
-    private int compareToArray(int[] array, int[] table) {
+    private int compareTwoArray(int[] array, int[] table) {
         int numOfSame = 0;
         for (int i = 0; i < array.length; i++) {
             int j = i;
@@ -20,30 +20,32 @@ public class Solution {
     }
 
     public int[] solution(int[] answers) {
-        int[] [] student = {STUDENT_A, STUDENT_B, STUDENT_C};
-        int max = compareToArray(answers, student[0]);
+        int[][] students = { STUDENT_A, STUDENT_B, STUDENT_C };
+        int[] correctNumbers = new int[3];
 
+        int correctNum = compareTwoArray(answers, students[0]);
+        correctNumbers[0] = correctNum;
+
+        int max = correctNum;
         int numOfMax = 0;
-        int[] temp = new int[3];
-        for (int i = 0; i <student.length; i++) {
-            int correctNum = compareToArray(answers, student[i]);
-            System.out.println(correctNum);
-            temp[i] = correctNum;
+
+        for (int i = 1; i < students.length; i++) {
+            correctNum = compareTwoArray(answers, students[i]);
+            correctNumbers[i] = correctNum;
             if (max < correctNum) {
                 max = correctNum;
                 numOfMax = 1;
-            } else if(max == correctNum) {
+            } else if (max == correctNum) {
                 numOfMax++;
             }
         }
         int[] maxCorrectStudent = new int[numOfMax];
         int index = 0;
-        for (int i = 0; i < temp.length; i++) {
-            if(temp[i] == max) {
+        for (int i = 0; i < correctNumbers.length; i++) {
+            if (correctNumbers[i] == max) {
                 maxCorrectStudent[index++] = i + 1;
             }
         }
-        Arrays.sort(maxCorrectStudent);
         return maxCorrectStudent;
     }
 
