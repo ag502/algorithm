03e49fd0b04c1b1f1077f176a92c936b39cdb983
ag502 <ag502@naver.com>
2019-10-04 17:35:20 +0900

    Make Student Class

--- Problem/MockTest/src/Solution.java
+++ Problem/MockTest/src/Solution.java
@@ -1,41 +1,71 @@
 import java.util.*;
 
-public class Solution {
-    private final int[] STUDENT_A = { 1, 2, 3, 4, 5 };
-    private final int[] STUDENT_B = { 2, 1, 2, 3, 2, 4, 2, 5 };
-    private final int[] STUDENT_C = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+class Student {
+    private int[] pattern;
+    private int correctNum;
 
-    private int compareTwoArray(int[] array, int[] table) {
-        int numOfSame = 0;
-        for (int i = 0; i < array.length; i++) {
-            int j = i;
-            if (j >= table.length) {
-                j = i % table.length;
-            }
-            if (array[i] == table[j]) {
-                numOfSame++;
-            }
-        }
-        return numOfSame;
+    Student(int[] pattern) {
+        this.pattern = pattern;
+        correctNum = 0;
     }
 
+    public int compareTwoArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int j = i;
+            if (j >= this.pattern.length) {
+                j = i % this.pattern.length;
+            }
+            if (array[i] == this.pattern[j]) {
+                correctNum++;
+            }
+        }
+        return correctNum;
+    }
+
+    public int getCorrectNum() {
+        return this.correctNum;
+    }
+}
+
+public class Solution {
+    private final int[] STUDENT_A_PATTERN = { 1, 2, 3, 4, 5 };
+    private final int[] STUDENT_B_PATTERN = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    private final int[] STUDENT_C_PATTERN = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+
+//    private int compareTwoArray(int[] array, int[] table) {
+//        int numOfSame = 0;
+//        for (int i = 0; i < array.length; i++) {
+//            int j = i;
+//            if (j >= table.length) {
+//                j = i % table.length;
+//            }
+//            if (array[i] == table[j]) {
+//                numOfSame++;
+//            }
+//        }
+//        return numOfSame;
+//    }
+
     public int[] solution(int[] answers) {
-        int[][] students = { STUDENT_A, STUDENT_B, STUDENT_C };
-        int[] correctNumbers = new int[3];
+        Student stuA = new Student(STUDENT_A_PATTERN);
+        Student stuB = new Student(STUDENT_B_PATTERN);
+        Student stuC = new Student(STUDENT_C_PATTERN);
 
-        int correctNum = compareTwoArray(answers, students[0]);
-        correctNumbers[0] = correctNum;
+        stuA.compareTwoArray(answers);
+        stuB.compareTwoArray(answers);
+        stuC.compareTwoArray(answers);
 
-        int max = correctNum;
-        int numOfMax = 0;
+        int[] correctNumbers = {stuA.getCorrectNum(), stuB.getCorrectNum(), stuC.getCorrectNum()};
 
-        for (int i = 1; i < students.length; i++) {
-            correctNum = compareTwoArray(answers, students[i]);
-            correctNumbers[i] = correctNum;
-            if (max < correctNum) {
-                max = correctNum;
+        int max = correctNumbers[0];
+        int numOfMax = 1;
+
+        for (int i = 1; i < correctNumbers.length; i++) {
+            if (max < correctNumbers[i]) {
+                max = correctNumbers[i];
                 numOfMax = 1;
-            } else if (max == correctNum) {
+            } else if (max == correctNumbers[i]) {
                 numOfMax++;
             }
         }
@@ -51,7 +81,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] a1 = {1, 2, 3, 4, 5};
-        int[] a2 = {3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2, 4, 2, 1, 3, 2};
+        int[] a2 = {1, 3, 2, 4, 2};
         System.out.println(Arrays.toString(new Solution().solution(a1)));
         System.out.println(Arrays.toString(new Solution().solution(a2)));
     }
