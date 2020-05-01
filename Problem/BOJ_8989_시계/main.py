e2d018343fa9ef53c46e3ade6194083a68622540
ag502 <ag502@naver.com>
2020-05-01 22:22:30 +0900

    BOJ 8989

--- Problem/BOJ_8989_시계/main.py
+++ Problem/BOJ_8989_시계/main.py
@@ -4,17 +4,12 @@ def time_parsing(time):
     hour, minute = list(map(int, time.split(":")))
 
     converted_hour = hour
-    if hour > 12:
+    if hour >= 12:
         converted_hour = hour - 12
 
-    if minute == 0:
-        converted_minute = 12
-    else:
-        converted_minute = round(minute / 5, 2)
-
-    degree = abs(converted_hour - converted_minute)
-    if degree > 6:
-        degree = 12 - degree
+    degree = abs(converted_hour * 30 - minute * 5.5)
+    if degree > 180:
+        degree = 360 - degree
     return hour, degree
 
 def main():
@@ -30,9 +25,9 @@ def main():
         temp.sort(key=lambda x: (x[2], x[1]))
         sorted_degree.append(temp)
 
-    # for case in sorted_degree:
-    #     print(case[2][0])
-    print(sorted_degree)
+    for case in sorted_degree:
+        print(case[2][0])
+    # print(sorted_degree)
 
 if __name__ == "__main__":
     main()
