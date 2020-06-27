765464a92d636595c56d37b6e148ab5199fb7619
ag502 <ag502@naver.com>
2020-06-28 01:17:55 +0900

    BOJ 5639 unresolved

--- Problem/BOJ_5639_이진 검색 트리/main.py
+++ Problem/BOJ_5639_이진 검색 트리/main.py
@@ -1,63 +1,34 @@
 from sys import stdin, setrecursionlimit
-setrecursionlimit(10**9)
+setrecursionlimit(10 ** 9)
 
-class BST:
-    class Node:
-        def __init__(self, data):
-            self.left = None
-            self.right = None
-            self.parent = None
-            self.data = data
+def post_order(node_list):
+    if len(node_list) == 0:
+        return []
 
-    def __init__(self):
-        self.root = None
-        self.size = 0
-
-    def search(self, start):
-        pass
-
-    def add(self, data):
-        new_node = self.Node(data)
-        if self.size == 0:
-            self.root = new_node
+    root = node_list[0]
+    if len(node_list) == 1:
+        return [root]
+    left = []
+    right = []
+    for i in range(1, len(node_list)):
+        if root > node_list[i]:
+            left.append(node_list[i])
         else:
-            current_node = self.root
-            prev_node = None
-            is_left = False
-            while current_node is not None:
-                prev_node = current_node
-                if current_node.data < data:
-                    current_node = current_node.right
-                    is_left = False
-                elif current_node.data > data:
-                    current_node = current_node.left
-                    is_left = True
+            right.append(node_list[i])
+    return post_order(left) + post_order(right) + [root]
 
-            if is_left:
-                prev_node.left = new_node
-            else:
-                prev_node.right = new_node
-            new_node.parent = prev_node
-        self.size += 1
-
-    def post_order(self, start_point):
-        if start_point is None:
-            return
-        self.post_order(start_point.left)
-        self.post_order(start_point.right)
-        print(start_point.data)
 
 def main():
-    binary_search_tree = BST()
+    node_list = []
     while True:
         try:
-            data = int(stdin.readline())
-            binary_search_tree.add(data)
+            node_list.append(int(stdin.readline()))
         except:
             break
 
-    binary_search_tree.post_order(binary_search_tree.root)
-
+    number_list = post_order(node_list)
+    for num in number_list:
+        print(num)
 
 if __name__ == '__main__':
     main()
