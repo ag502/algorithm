3cd61a2be77174027b92f185c8d487c5746ad8a8
ag502 <ag502@naver.com>
2020-06-30 18:27:35 +0900

    Prgrms_124 나라의 숫자

--- Problem/Prgrms_124 나라의 숫자/main.py
+++ Problem/Prgrms_124 나라의 숫자/main.py
@@ -1,15 +1,11 @@
 num_list = [1, 2, 4]
 
 def get_124_number(n):
-    if n == 1 or n == 2 or n == 3:
+    if 0 < n <= 3:
         return str(num_list[n - 1])
-    q = n % 3
-    n = n // 3
-    if q == 0:
-        n -= 1
-        q = 4
 
-    return get_124_number(n) + str(q)
+    p, q = divmod(n - 1, 3)
+    return get_124_number(p) + str(num_list[q])
 
 def solution(n):
     answer = get_124_number(n)
