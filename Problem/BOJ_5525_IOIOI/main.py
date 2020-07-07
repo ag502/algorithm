6940399d275cf56ae66d449868cd2c44d9c27338
ag502 <ag502@naver.com>
2020-07-08 01:03:59 +0900

    BOJ 5525 :white_check_mark:

--- Problem/BOJ_5525_IOIOI/main.py
+++ Problem/BOJ_5525_IOIOI/main.py
@@ -2,38 +2,22 @@ from sys import stdin
 
 def main():
     n = int(stdin.readline())
-    length_of_s = int(stdin.readline())
+    length_of_string = int(stdin.readline())
     s = stdin.readline().rstrip()
 
     answer = 0
-    index = 0
-
-    while True:
-        is_wrong = False
-        if index > len(s) - (2 * n + 1):
-            break
-        if s[index] == "I" and s[index + 1] == "O":
-            is_I = False
-            for i in range(2 * n - 1):
-                if s[index + 2 + i] == "I":
-                    if is_I:
-                        is_wrong = True
-                        break
-                    else:
-                        is_I = not is_I
-                elif s[index + 2 + i] == "O":
-                    if not is_I:
-                        is_wrong = True
-                        break
-                    else:
-                        is_I = not is_I
-
-            if not is_wrong:
+    pattern_cnt = 0
+    idx = 1
+    while idx <= length_of_string - 2:
+        if s[idx - 1] == 'I' and s[idx] == 'O' and s[idx + 1] == 'I':
+            pattern_cnt += 1
+            if pattern_cnt == n:
                 answer += 1
-            index += 2 * n
-            continue
-        index += 1
-
+                pattern_cnt -= 1
+            idx += 1
+        else:
+            pattern_cnt = 0
+        idx += 1
     print(answer)
 
 if __name__ == '__main__':
