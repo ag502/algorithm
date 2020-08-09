0db43cba66d24aa85b40393cd01efff47a6fa3cb
ag502 <ag502@naver.com>
2020-08-09 23:29:49 +0900

    BOJ 10869 사칙연산

---
+++ Problem/BOJ_10869_사칙연산/main.js
@@ -0,0 +1,16 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const [a, b] = line.split(" ").map((el) => parseInt(el));
+  console.log(a + b);
+  console.log(a - b);
+  console.log(a * b);
+  console.log(Math.floor(a / b));
+  console.log(a % b);
+  rl.close();
+});
