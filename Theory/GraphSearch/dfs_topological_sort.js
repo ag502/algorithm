281db656fd7217380bb598b459dd626d5c9b3807
ag502 <ag502@naver.com>
2020-08-17 22:08:31 +0900

    graph dfs topological sort

---
+++ Theory/GraphSearch/dfs_topological_sort.js
@@ -0,0 +1,49 @@
+const adj = new Map();
+adj.set("6", ["7", "8", "9"]);
+adj.set("7", ["1"]);
+adj.set("8", ["2"]);
+adj.set("9", ["4"]);
+adj.set("1", ["2"]);
+adj.set("2", ["3"]);
+adj.set("10", ["3"]);
+adj.set("3", ["4"]);
+adj.set("4", ["5"]);
+adj.set("5", []);
+
+const visited = new Map();
+adj.forEach((_, vertex) => {
+  visited.set(vertex, false);
+});
+
+console.log([...adj]);
+console.log(adj.entries());
+
+const records = [];
+
+const dfs = (here) => {
+  // 1. 체크인
+  visited.set(here, true);
+  // 2. 도착
+  console.log(`dfs visits ${here}`);
+  // 3. 인접한 정점 순회
+  adj.get(here).forEach((there) => {
+    // 4. 갈 수 있는 지 검사
+    if (visited.get(there) === false) {
+      // 5. 간다
+      dfs(there);
+      //   records.push(there);
+    }
+  });
+  records.push(here);
+};
+
+const dfsAll = () => {
+  for (let vertex of adj.keys()) {
+    if (visited.get(vertex) === false) {
+      dfs(vertex);
+    }
+  }
+};
+
+dfsAll();
+console.log(records);
