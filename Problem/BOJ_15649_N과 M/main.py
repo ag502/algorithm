0cc2e39acc80177db811958698ef115cec9dc7ef
ag502 <ag502@naver.com>
2020-08-25 21:31:34 +0900

    BOJ 15649

--- Problem/BOJ_15649_N과 M/main.py
+++ Problem/BOJ_15649_N과 M/main.py
@@ -9,20 +9,20 @@ def main():
         permutation(i, 0, N, M, [], visited)
 
 
-def permutation(cur_num, selected_num, N, M, answer, visited):
+def permutation(cur_num, selected_num, n, m, answer, visited):
     # 1. 방문
     selected_num += 1
     visited[cur_num] = -1
     # 2. 도착
     answer.append(cur_num)
     # 3. 인접 노드 순회
-    for i in range(1, N + 1):
+    for i in range(1, n + 1):
         # 4. 갈 수 있는지 검사
-        if selected_num < M and visited[i] != -1 and i != cur_num:
+        if selected_num < m and visited[i] != -1 and i != cur_num:
             # 5. 간다
-            permutation(i, selected_num, N, M, answer, visited)
+            permutation(i, selected_num, n, m, answer, visited)
     # 5. 체크아웃
-    if len(answer) == M:
+    if len(answer) == m:
         print(" ".join(map(str, answer)))
     answer.pop()
     selected_num -= 1
