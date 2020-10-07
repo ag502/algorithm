6e78019f50729c44be22f1ad7f474209be3042e4
ag502 <ag502@naver.com>
2020-10-08 00:23:13 +0900

    BOJ 11266 :white_check_mark:

--- Problem/BOJ_11266_단절점/main.py
+++ Problem/BOJ_11266_단절점/main.py
@@ -1,11 +1,15 @@
-from sys import stdin
+from sys import stdin, setrecursionlimit
+setrecursionlimit(10000)
+
+count = 0
 
 def main():
     v, e = map(int, stdin.readline().split())
     graph = {}
     for i in range(1, v + 1):
         graph[i] = []
-    visited = [False] * (v + 1)
+    discover = [False] * (v + 1)
+    is_cut_vertex = [False] * (v + 1)
 
     for _ in range(e):
         a, b = map(int, stdin.readline().split())
@@ -13,23 +17,43 @@ def main():
         graph[b].append(a)
 
     for i in range(1, v + 1):
-        if not visited[i]:
-            dfs(graph, visited, i)
+        if discover[i] == 0:
+           dfs(graph, discover, i, True, is_cut_vertex)
 
-    # print(graph)
+    total_count = 0
+    answer = []
+    for idx, is_cut in enumerate(is_cut_vertex[1:]):
+        if is_cut:
+            total_count += 1
+            answer.append(str(idx + 1))
 
-def dfs(graph, visited, here):
+    print(total_count)
+    print(' '.join(answer))
+
+
+
+def dfs(graph, discover, here, is_root, is_cut_vertex):
     # 1. 방문
-    visited[here] = True
-    # 2. 목적지?
-    print(here)
-    # 3. 갈 수 있는  주변 노드 탐색
-    for there in graph[here]:
-        # 4. 갈 수 있는지 검사
-        if not visited[there]:
-            # 5. 간다
-            dfs(graph, visited, there)
+    global count
+    count += 1
+    discover[here] = count
+    ret = discover[here]
+    child = 0
 
+    # 2. 갈 수 있는 곳을 탐색
+    for there in graph[here]:
+        if discover[there] != 0:
+            ret = min(ret, discover[there])
+        else:
+            child += 1
+            low = dfs(graph, discover, there, False, is_cut_vertex)
+            if not is_root and low >= discover[here]:
+                is_cut_vertex[here] = True
+            ret = min(ret, low)
+
+    if is_root:
+        is_cut_vertex[here] = (child >= 2)
+    return ret
 
 
 if __name__ == '__main__':
