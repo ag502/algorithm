fe2b43f3b8ff230e933a006a0cd16242bc3276b3
ag502 <ag502@naver.com>
2020-10-14 02:45:37 +0900

    BOJ 1504 :white_check_mark:

--- Problem/BOJ_1504_특정한 최단 경로/main.py
+++ Problem/BOJ_1504_특정한 최단 경로/main.py
@@ -3,56 +3,47 @@ from collections import deque
 
 def main():
     n, e = map(int, stdin.readline().split())
-    # graph = {}
-    # for i in range(1, n + 1):
-    #     graph[i] = []
-    #
-    # dist = [float('INF')] * (n + 1)
-    # pq = deque()
-    #
-    # for _ in range(e):
-    #     a, b, c = map(int, stdin.readline().split())
-    #     graph[a].append([b, c])
-    #     graph[b].append([a, c])
-    #
-    # # Dijkstra
-    # pq.append([1, 0])
-    # dist[1] = 0
-    #
-    # while len(pq) != 0:
-    #     cur_vertex, cur_distance = pq.popleft()
-    #
-    #     if cur_distance > dist[cur_vertex]:
-    #         continue
-    #
-    #     for next_vertex, next_distance in graph[cur_vertex]:
-    #         if dist[next_vertex] > next_distance + cur_distance:
-    #             dist[next_vertex] = next_distance + cur_distance
-    #             pq.append([next_vertex, next_distance + cur_distance])
-    #
-    # print(dist)
-
-    # Floyd-Warshall
-    graph = [[float('INF')] * (n + 1) for _ in range(n + 1)]
+    graph = {}
     for i in range(1, n + 1):
-        graph[i][i] = 0
+        graph[i] = []
 
     for _ in range(e):
         a, b, c = map(int, stdin.readline().split())
-        graph[a][b] = c
-        graph[b][a] = c
+        graph[a].append([b, c])
+        graph[b].append([a, c])
 
-    stop_by = list(map(int, stdin.readline().split()))
+    stop_by_1, stop_by_2 = map(int, stdin.readline().split())
 
-    for k in stop_by:
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    dist_0 = dijkstra(1, graph, n)
+    dist_1 = dijkstra(stop_by_1, graph, n)
+    dist_2 = dijkstra(stop_by_2, graph, n)
 
-    print(graph)
-    answer = min(graph[1][stop_by[0]] + graph[stop_by[0]][stop_by[1]] + graph[stop_by[1]][n],
-                 graph[1][stop_by[1]] + graph[stop_by[0]][stop_by[1]] + graph[stop_by[0]][n])
-    print(answer if answer != float('INF') else -1)
+    # print(dist_0, dist_1, dist_2)
+
+    answer = min(dist_0[stop_by_1] + dist_1[stop_by_2] + dist_2[n],
+                 dist_0[stop_by_2] + dist_2[stop_by_1] + dist_1[n])
+
+    print(answer if answer != float('inf') else -1)
+
+def dijkstra(start_vertex, graph, n):
+    dist = [float('inf')] * (n + 1)
+    pq = deque()
+
+    dist[start_vertex] = 0
+    pq.append([start_vertex, 0])
+
+    while len(pq) != 0:
+        cur_vertex, cur_distance = pq.popleft()
+
+        if cur_distance > dist[cur_vertex]:
+            continue
+
+        for next_vertex, next_distance in graph[cur_vertex]:
+            if dist[next_vertex] > dist[cur_vertex] + next_distance:
+                dist[next_vertex] = dist[cur_vertex] + next_distance
+                pq.append([next_vertex, dist[next_vertex]])
+
+    return dist
 
 if __name__ == '__main__':
     main()
