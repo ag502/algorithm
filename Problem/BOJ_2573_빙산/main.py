ae4921ad3bcd92aeb9c6670c9dc3f78a3d471d3c
ag502 <ag502@naver.com>
2020-10-21 23:32:55 +0900

    BOJ 2573 :white_check_mark:

--- Problem/BOJ_2573_빙산/main.py
+++ Problem/BOJ_2573_빙산/main.py
@@ -1,12 +1,11 @@
 from sys import stdin, setrecursionlimit
 from collections import deque
-setrecursionlimit(1000000)
+setrecursionlimit(10000)
 
 directions = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
-
 def main():
-    stdin = open("./test_case.txt", 'r')
+    # stdin = open("./test_case.txt", 'r')
     rows, cols = map(int, stdin.readline().split())
     icy = []
 
@@ -14,11 +13,12 @@ def main():
         row = list(map(int, stdin.readline().split()))
         icy.append(row)
 
+    queue = deque()
     year = 0
     while True:
-        chunk = 0
         visited = [[False] * cols for _ in range(rows)]
-
+        queue.clear()
+        chunk = 0
         for row in range(rows):
             for col in range(cols):
                 if icy[row][col] != 0 and not visited[row][col]:
@@ -28,44 +28,36 @@ def main():
         if chunk >= 2:
             print(year)
             return
-
         if chunk == 0:
             print(0)
             return
 
-        melting(icy, rows, cols)
+        melt(icy, rows, cols)
         year += 1
 
-
-def melting(graph, rows, cols):
-    queue = deque()
+def melt(graph, rows, cols):
+    visited = [[False] * cols for _ in range(rows)]
     for row in range(rows):
         for col in range(cols):
-            if graph[row][col] != 0:
-                num_of_sea = 0
-                for moving_row, moving_col in directions:
-                    if 0 <= row + moving_row < rows and 0 <= col + moving_col < cols:
-                        if graph[row + moving_row][col + moving_col] == 0:
-                            num_of_sea += 1
-
-                queue.append([row, col, num_of_sea])
-
-    while len(queue) != 0:
-        cur_row, cur_col, cur_num_of_sea = queue.pop()
-        if graph[cur_row][cur_col] - cur_num_of_sea < 0:
-            graph[cur_row][cur_col] = 0
-        else:
-            graph[cur_row][cur_col] -= cur_num_of_sea
+            num_of_sea = 0
+            for moving_row, moving_col in directions:
+                sea_row = row + moving_row
+                sea_col = col + moving_col
+                if 0 <= sea_row < rows and 0 <= sea_col < cols:
+                    if not visited[sea_row][sea_col] and graph[sea_row][sea_col] == 0:
+                        num_of_sea += 1
+            if graph[row][col] > 0:
+                graph[row][col] = max(0, graph[row][col] - num_of_sea)
+                visited[row][col] = True
 
 def dfs(graph, cur_row, cur_col, rows, cols, visited):
-    # 방문
     visited[cur_row][cur_col] = True
 
     for moving_row, moving_col in directions:
         next_row = cur_row + moving_row
         next_col = cur_col + moving_col
         if 0 <= next_row < rows and 0 <= next_col < cols:
-            if graph[next_row][next_col] != 0 and not visited[next_row][next_col]:
+            if not visited[next_row][next_col] and graph[next_row][next_col] != 0:
                 dfs(graph, next_row, next_col, rows, cols, visited)
 
 if __name__ == '__main__':
