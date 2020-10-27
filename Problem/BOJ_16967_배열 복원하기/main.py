d0373a30fa9fc425d9b86b5ea36f050c237bc6ac
ag502 <ag502@naver.com>
2020-10-27 23:40:02 +0900

    BOJ 16967 :white_check_mark:

--- Problem/BOJ_16967_배열 복원하기/main.py
+++ Problem/BOJ_16967_배열 복원하기/main.py
@@ -10,26 +10,17 @@ def main():
         row = list(map(int, stdin.readline().split()))
         array_b.append(row)
 
-    array_a_1 = [[0] * cols for _ in range(rows)]
-    array_a_2 = [[0] * cols for _ in range(rows)]
-
-    for i in range(rows):
-        for j in range(cols):
-            array_a_1[i][j] = array_b[i][j]
-
-    for i in range(x, rows + x):
-        for j in range(y, cols + y):
-            array_a_2[i - x][j - y] = array_b[i][j]
-
     answer = [[0] * cols for _ in range(rows)]
+
     for i in range(rows):
         for j in range(cols):
-            if array_a_1[i][j] == array_a_2[i][j]:
-                answer[i][j] = array_a_1[i][j]
+            if x <= i < rows and y <= j < cols:
+                answer[i][j] = abs(array_b[i][j] - answer[i - x][j - y])
             else:
-                answer[i][j] = abs(array_a_1[i][j] - array_a_2[i][j])
+                answer[i][j] = array_b[i][j]
 
-    print(answer)
-    
+    for i in range(rows):
+        print(' '.join(map(str, answer[i])))
+
 if __name__ == '__main__':
     main()
