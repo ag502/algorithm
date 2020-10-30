bd75b7f60c9614b6a03f24006796060994a57545
ag502 <ag502@naver.com>
2020-10-30 22:40:45 +0900

    BOJ 14888 solved without permutation

--- Problem/BOJ_14888_연산자 끼워넣기/main.py
+++ Problem/BOJ_14888_연산자 끼워넣기/main.py
@@ -14,8 +14,25 @@ def do_operation(num1, num2, operator):
         else:
             return num1 // num2
 
+def dfs(operators, visited, start_idx, answer, temp):
+    # 1. 방문
+    visited[start_idx] = True
+    # 2. 도착
+    temp.append(operators[start_idx])
+    # 3. 주변 탐색
+    for next_idx in range(len(operators)):
+        # 4. 갈 수 있는지 확인
+        if not visited[next_idx]:
+            dfs(operators, visited, next_idx, answer, temp)
+    # 5. 체크아웃
+    if len(temp) == len(operators):
+        answer.append(''.join(temp))
+    # visited[start_idx] = False
+    temp.pop()
 
 def main():
+    stdin = open('./test_case.txt', 'r')
+
     n = int(stdin.readline())
     numbers = list(map(int, stdin.readline().split()))
     operators = list(map(int, stdin.readline().split()))
@@ -30,10 +47,22 @@ def main():
         else:
             operator_list += ['/'] * operator
 
-    operator_list = set(permutations(operator_list))
+
+    # backtracking
+    visited = [False] * len(operator_list)
+    # print(operator_list)
+    # print(visited)
+    permute = []
+    for idx in range(len(operator_list)):
+        dfs(operator_list, visited, idx, permute, [])
+
+    permute = set(permute)
+    print(permute)
+
     max_res = -1000000001
     min_res = 1000000001
-    for operator in operator_list:
+    for operator in permute:
+        operator = list(operator)
         result = do_operation(numbers[0], numbers[1], operator[0])
         for i in range(1, len(operator)):
             result = do_operation(result, numbers[i + 1], operator[i])
@@ -41,6 +70,19 @@ def main():
             max_res = result
         if min_res > result:
             min_res = result
+
+    # python permutation
+    # operator_list = set(permutations(operator_list))
+    # max_res = -1000000001
+    # min_res = 1000000001
+    # for operator in operator_list:
+    #     result = do_operation(numbers[0], numbers[1], operator[0])
+    #     for i in range(1, len(operator)):
+    #         result = do_operation(result, numbers[i + 1], operator[i])
+    #     if max_res < result:
+    #         max_res = result
+    #     if min_res > result:
+    #         min_res = result
 
     print(max_res)
     print(min_res)
