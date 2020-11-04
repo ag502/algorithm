83d3f49231726ec8d293feed3ef0e7756ed4fbcf
ag502 <ag502@naver.com>
2020-11-05 06:52:19 +0900

    BOJ 10157 :white_check_mark:

--- Problem/BOJ_10157_자리배정/main.py
+++ Problem/BOJ_10157_자리배정/main.py
@@ -1,21 +1,47 @@
 from sys import stdin
 
+moving_dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
 def main():
+    stdin = open('./test_case.txt', 'r')
     cols, rows = map(int, stdin.readline().split())
-    seat = int(stdin.readline().rstrip())
-    stage_edge = cols * 2 + (rows - 2) * 2
-    stage = [[0] * (cols + 1) for _ in range(rows + 1)]
+    waiting_num = int(stdin.readline())
+    seats = [[0] * cols for _ in range(rows)]
 
-    for y in range(rows, 0, -1):
-        for x in range(1, cols + 1):
-            if y == 1:
-                stage[y][x] = rows + (x - 1)
-            elif x == 1:
-                stage[y][x] = (rows + 1) - y
+    if waiting_num > cols * rows:
+        print(0)
+        return
+
+    current_row = rows - 1
+    current_col = 0
+    seats[current_row][current_col] = 1
+
+    if waiting_num == 1:
+        print(str(current_col + 1) + ' ' + str(rows - current_row))
+        return
+
+    number = 2
+    count = 0
+    for i in range(rows * cols):
+        temp_row = current_row + moving_dir[count % 4][0]
+        temp_col = current_col + moving_dir[count % 4][1]
+        if 0 <= temp_row < rows and 0 <= temp_col < cols:
+            if seats[temp_row][temp_col] == 0:
+                current_row = temp_row
+                current_col = temp_col
             else:
-                stage[y][x] = stage[y][1] + stage_edge - (x - 1)
-
-    print(stage)
+                count += 1
+                current_row += moving_dir[count % 4][0]
+                current_col += moving_dir[count % 4][1]
+        else:
+            count += 1
+            current_row += moving_dir[count % 4][0]
+            current_col += moving_dir[count % 4][1]
+        if waiting_num == number:
+            print(str(current_col + 1) + ' ' + str(rows - current_row))
+            return
+        seats[current_row][current_col] = number
+        number += 1
 
 if __name__ == '__main__':
     main()
