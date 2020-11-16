71df6e11322c37c60a4f71c36f4fd604e219f2dc
ag502 <ag502@naver.com>
2020-11-16 23:44:18 +0900

    BOJ 17352

--- Problem/BOJ_17352_여러분의 다리가 되어 드리겠습니다!/main.py
+++ Problem/BOJ_17352_여러분의 다리가 되어 드리겠습니다!/main.py
@@ -7,16 +7,31 @@ def find(island, parents):
     parents[island] = find(parents[island], parents)
     return parents[island]
 
+def merge(island_1, island_2, parents, ranks):
+    island_1_root = find(island_1, parents)
+    island_2_root = find(island_2, parents)
+    if island_1_root == island_2_root:
+        return
+
+    if ranks[island_2_root] > ranks[island_1_root]:
+        temp = ranks[island_1_root]
+        ranks[island_1_root] = ranks[island_2_root]
+        ranks[island_2_root] = temp
+
+    parents[island_2_root] = island_1_root
+
+    if ranks[island_1_root] == ranks[island_2_root]:
+        ranks[island_1_root] += 1
+
 def main():
     stdin = open('./test_case.txt', 'r')
     islands = int(stdin.readline())
     parents = [i for i in range(islands + 1)]
+    ranks = [0] * (islands + 1)
 
     for _ in range(islands - 2):
         island_1, island_2 = map(int, stdin.readline().split())
-        island_1_root = find(island_1, parents)
-        island_2_root = find(island_2, parents)
-        parents[island_2_root] = island_1_root
+        merge(island_1, island_2, parents, ranks)
 
     roots = set()
     for island in parents[1:]:
