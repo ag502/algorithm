3d347f7c5070d4632ee3b920738e60a40d1c3989
ag502 <ag502@naver.com>
2020-11-20 22:26:34 +0900

    BOJ 1001

---
+++ Problem/BOJ_1001_A-B/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src\\input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        System.out.println(num1 - num2);
+    }
+}
