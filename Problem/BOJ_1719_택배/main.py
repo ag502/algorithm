e117364cd5451ba697fa4fa8de33936a8adf18da
ag502 <ag502@naver.com>
2020-11-25 23:24:55 +0900

    BOJ 1719 unresolved

--- Problem/BOJ_1719_택배/main.py
+++ Problem/BOJ_1719_택배/main.py
@@ -1,37 +1,42 @@
 from sys import stdin
+from heapq import heappush, heappop
 
 def main():
     stdin = open('./input.txt', 'r')
     num_of_stock_places, num_of_paths = map(int, stdin.readline().split())
-
-    stock_places = [[float('inf')] * (num_of_stock_places + 1) for _ in range(num_of_stock_places + 1)]
-    for i in range(num_of_stock_places + 1):
-        stock_places[i][i] = 0
+    stock_places = {}
+    for i in range(1, num_of_stock_places + 1):
+        stock_places[i] = []
 
     for _ in range(num_of_paths):
-        start, finish, time = map(int, stdin.readline().split())
-        stock_places[start][finish] = time
-        stock_places[finish][start] = time
+        p1, p2, distance = map(int, stdin.readline().split())
+        stock_places[p1].append([p2, distance])
+        stock_places[p2].append([p1, distance])
 
-    stop_places = [[0] * (num_of_stock_places + 1) for _ in range(num_of_stock_places + 1)]
+    answer = []
+    for stock_place in range(1, num_of_stock_places + 1):
+        heap = []
+        stop_places = []
+        dist = [float('inf')] * (num_of_stock_places + 1)
 
-    for k in range(1, num_of_stock_places + 1):
-        for i in range(1, num_of_stock_places + 1):
-            if stock_places[i][k] == float('inf'):
+        heappush(heap, [0, stock_place])
+        dist[stock_place] = 0
+
+        while len(heap) != 0:
+            cur_dist, cur_place = heappop(heap)
+
+            if cur_dist > dist[cur_place]:
                 continue
-            for j in range(1, num_of_stock_places + 1):
-                if stock_places[i][k] + stock_places[k][j] < stock_places[i][j]:
-                    stock_places[i][j] = stock_places[i][k] + stock_places[k][j]
-                    if stop_places[i][j] == 0:
-                        stop_places[i][j] = k
-                #     print('1 ' + str(k))
-                elif stock_places[i][k] + stock_places[k][j] > stock_places[i][j]:
-                    if stop_places[i][j] == 0:
-                        stop_places[i][j] = j
-                    # print('2 ' + str(j))
-    print(stock_places)
-    print(stop_places)
 
+            for next_place, next_dist in stock_places[cur_place]:
+                if dist[next_place] > next_dist + cur_dist:
+                    dist[next_place] = next_dist + cur_dist
+                    heappush(heap, [next_dist + cur_dist, next_place])
+                    stop_places.append([cur_place, next_place])
+        print(dist)
+        answer.append(stop_places)
+
+    print(answer)
 
 if __name__ == '__main__':
     main()
