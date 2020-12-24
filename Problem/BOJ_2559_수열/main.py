dcde5fbb99b458340076f839cfaebb55dd3f27bf
LeeJaeYun <ag502@naver.com>
2020-12-24 12:50:34 +0900

    BOJ 2559 dp :white_check_mark: :fire:

--- Problem/BOJ_2559_수열/main.py
+++ Problem/BOJ_2559_수열/main.py
@@ -1,18 +1,22 @@
 from sys import stdin
 
+MAX = 100001
+
 def main():
     stdin = open('./input.txt', 'r')
     total_days, standard_days = map(int, stdin.readline().split())
     temperature = list(map(int, stdin.readline().split()))
 
-    sum_of_sub_temperature = []
-    for i in range(len(temperature) - standard_days + 1):
-        temp = 0
-        for j in range(standard_days):
-            temp += temperature[i + j]
-        sum_of_sub_temperature.append(temp)
+    dp = [-987654321] * MAX
+    sum_of_temperature = 0
+    for day in range(standard_days):
+        sum_of_temperature += temperature[day]
+    dp[0] = sum_of_temperature
 
-    print(max(sum_of_sub_temperature))
+    for day in range(1, total_days - (standard_days - 1)):
+        dp[day] = dp[day - 1] - temperature[day - 1] + temperature[day + (standard_days - 1)]
+
+    print(max(dp))
 
 if __name__ == '__main__':
     main()
