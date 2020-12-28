4ac96a609a9287d71758bccce38fb3c41845e387
LeeJaeYun <ag502@naver.com>
2020-12-28 17:51:38 +0900

    BOJ 2343 :white_check_mark: :fire:

--- Problem/BOJ_2343_기타레슨/main.py
+++ Problem/BOJ_2343_기타레슨/main.py
@@ -6,30 +6,23 @@ def main():
     num_of_lessons, num_of_blue_ray = map(int, stdin.readline().split())
     lessons = list(map(int, stdin.readline().split()))
 
-    start = lessons[num_of_lessons - 1]
+    start = max(lessons)
     end = maxsize
 
     while start <= end:
         mid = (start + end) // 2
-        count = 0
+        count = 1
         total_lessons_time = 0
         for idx in range(num_of_lessons - 1):
             total_lessons_time += lessons[idx]
             if total_lessons_time + lessons[idx + 1] > mid:
                 total_lessons_time = 0
                 count += 1
-                print(mid, count)
 
-        if total_lessons_time != 0:
-            count += 1
-
-        if count < num_of_blue_ray:
+        if count <= num_of_blue_ray:
             end = mid - 1
-        elif count > num_of_blue_ray:
-            start = mid + 1
         else:
-            print(mid)
-            return
+            start = mid + 1
 
     print(start)
 
