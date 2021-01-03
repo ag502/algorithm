9ad6673a554a1870f2de4f5a7a61da9231422a9e
ag502 <ag502@naver.com>
2021-01-03 18:34:14 +0900

    BOJ 1000

---
+++ Problem/BOJ_1000_A+B/main.js
@@ -0,0 +1,8 @@
+const fs = require('fs')
+// const input = fs.readFileSync('./input.txt').toString().split(' ')
+const input = fs.readFileSync('./dev/stdin').toString().split(' ')
+
+const a = parseInt(input[0])
+const b = parseInt(input[1])
+
+console.log(a + b)
