0c4218efa50316be964b11b3358eb652b9b41660
LeeJaeYun <ag502@naver.com>
2021-01-16 14:27:25 +0900

    BOJ 3300 :white_check_mark:

--- Problem/BOJ_3300_무어 기계/main.js
+++ Problem/BOJ_3300_무어 기계/main.js
@@ -1,42 +1,31 @@
 const fs = require('fs')
-const input = fs.readFileSync("./input.txt").toString().trim().split("\n")
+const input = fs.readFileSync("./input.txt").toString().trim().split("\n").map(elem => elem.trim())
 
 const numOfMachines = parseInt(input[0])
 const machines = input.slice(1)
 
-function parsingMachine (machine) {
-    let newMachine = ""
-    Array.from(machine.trim()).forEach(char => {
-        if (char === "(") {
-            newMachine += char
-            newMachine += "?:"
-        } else if (char === "_") {
-            newMachine += "([A-Z])"
-        } else {
-            newMachine += char
-        }
-    })
-    return newMachine
-}
 
 for (let i = 0; i < numOfMachines * 2; i += 2) {
     const machine = machines[i]
     const output = machines[i + 1]
 
-    const newMachine = parsingMachine(machine)
-    // console.log(newMachine)
+    let matchCount = 0
+    let matchChar = ''
+    for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) {
+        const newMachine = machine.replace("_", String.fromCharCode(i))
+        const regex = new RegExp(`^${newMachine}$`)
 
-    const regex = new RegExp(newMachine)
-    if (!regex.test(output)) {
-        console.log("!")
-    } else {
-        const matchInfo = output.match(regex)
-        // console.log(matchInfo)
-
-        if (!matchInfo[1]) {
-            console.log("_")
-        } else {
-            console.log(matchInfo[1])
+        if (!!output.match(regex)) {
+            matchCount += 1
+            matchChar = String.fromCharCode(i)
         }
+    }
+
+    if (matchCount === 0) {
+        console.log("!")
+    } else if (matchCount > 1) {
+        console.log("_")
+    } else {
+        console.log(matchChar)
     }
 }
--- Problem/BOJ_3300_무어 기계/main.py
+++ Problem/BOJ_3300_무어 기계/main.py
@@ -2,19 +2,6 @@ from sys import stdin
 import re
 
 
-def parsing_machine(machine):
-    new_machine = ["^"]
-    for char in machine:
-        if char == "(":
-            new_machine.append("(?:")
-        elif char == "_":
-            new_machine.append("([A-Z])")
-        else:
-            new_machine.append(char)
-    new_machine.append("$")
-    return "".join(new_machine)
-
-
 def main():
     stdin = open('./input.txt', 'r')
     test_case = int(stdin.readline())
@@ -23,16 +10,22 @@ def main():
         machine = stdin.readline().strip()
         output = stdin.readline().strip()
 
-        regex = re.compile(parsing_machine(machine))
-        is_contained = regex.match(output)
+        match_count = 0
+        match_char = ''
+        for code in range(ord('A'), ord('Z') + 1):
+            new_machine = machine.replace("_", chr(code))
+            regex = re.compile("^" + new_machine + "$")
 
-        if is_contained is None:
+            if regex.match(output) is not None:
+                match_count += 1
+                match_char = chr(code)
+
+        if match_count == 0:
             print("!")
+        elif match_count == 1:
+            print(match_char)
         else:
-            if is_contained.group(1) is None:
-                print("_")
-            else:
-                print(is_contained.group(1))
+            print("_")
 
 
 if __name__ == '__main__':
