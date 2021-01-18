363cfbcf88c5522c51a4feb4ac5746dc362fc3a9
LeeJaeYun <ag502@naver.com>
2021-01-18 16:31:03 +0900

    Prgrms 후보키

--- Problem/Prgrms_후보키/main.py
+++ Problem/Prgrms_후보키/main.py
@@ -1,47 +1,57 @@
-def select_columns(total_columns, choose_number, visited, cur_col, candidate_keys, column_list, temp):
-    visited[cur_col] = True
-    temp.append(cur_col)
+def check_candidate_key(relations, keys):
+    tuple_set = set()
+    rows = len(relations)
+    for row in range(rows):
+        temp_string = ""
+        for key in keys:
+            temp_string += relations[row][key]
+        tuple_set.add(temp_string)
 
-    for next_col in range(cur_col + 1, total_columns):
-        if ''.join(map(str, temp)) in candidate_keys or str(next_col) in candidate_keys:
-            continue
-        if not visited[next_col] and len(temp) < choose_number:
-            select_columns(total_columns, choose_number, visited, next_col, candidate_keys, column_list, temp)
-
-    if len(temp) == choose_number:
-        column_list.append(temp[:])
-    temp.pop()
-    visited[cur_col] = False
+    if len(tuple_set) == rows:
+        return True
+    return False
 
 
-def check_is_candidate_key(relations, col_list, candidate_keys):
-    for cols in col_list:
-        tuple_set = set()
-        for relations_row in range(len(relations)):
-            temp = ""
-            for col_num in cols:
-                temp += relations[relations_row][col_num]
-            tuple_set.add(temp)
+def dfs(relations, num_of_attr, cur_key, key_temp, candidate_keys):
+    key_temp.append(cur_key)
 
-        if len(tuple_set) == len(relations):
-            candidate_keys.add(''.join(map(str, cols)))
+    if not check_candidate_key(relations, key_temp):
+        for next_key in range(cur_key + 1, num_of_attr):
+            dfs(relations, num_of_attr, next_key, key_temp, candidate_keys)
+    else:
+        candidate_keys.append(set(key_temp[:]))
+    key_temp.pop()
 
 
 def solution(relations):
-    num_of_attr = len(relations[0])
+    num_of_attributes = len(relations[0])
 
-    visited = [False] * num_of_attr
-    candidate_keys = set()
-    for choose_number in range(1, num_of_attr + 1):
-        col_list = []
-        for col in range(num_of_attr):
-            print(candidate_keys)
-            if str(col) not in candidate_keys:
-                select_columns(num_of_attr, choose_number, visited, col, candidate_keys, col_list, [])
-                check_is_candidate_key(relations, col_list, candidate_keys)
-        # print(col_list)
-    return len(candidate_keys)
+    candidate_keys = []
+
+    for attr in range(num_of_attributes):
+        dfs(relations, num_of_attributes, attr, [], candidate_keys)
+
+    candidate_keys.sort(key=lambda x: len(x))
+
+    # print(candidate_keys)
+
+    answer = []
+    for i in range(len(candidate_keys) - 1):
+        if candidate_keys[i] == -1:
+            continue
+        else:
+            answer.append(candidate_keys[i])
+        for j in range(i + 1, len(candidate_keys)):
+            if candidate_keys[j] != -1:
+                intersection = candidate_keys[i] & candidate_keys[j]
+                if len(intersection) == len(candidate_keys[i]):
+                    candidate_keys[j] = -1
+    if candidate_keys[len(candidate_keys) - 1] != -1:
+        answer.append(candidate_keys[len(candidate_keys) - 1])
+
+    return len(answer)
 
 
-print(solution([["100", "ryan", "music", "2", "a"], ["200", "apeach", "math", "2", "b"], ["300", "tube", "computer", "3", "c"],
-                ["400", "con", "computer", "4", "d"], ["500", "muzi", "music", "3", "e"], ["600", "apeach", "music", "2", "f"]]))
+print(solution(
+    [["100", "ryan", "music", "2", "a"], ["200", "apeach", "math", "2", "b"], ["300", "tube", "computer", "3", "c"],
+     ["400", "con", "computer", "4", "d"], ["500", "muzi", "music", "3", "e"], ["600", "apeach", "music", "2", "f"]]))
