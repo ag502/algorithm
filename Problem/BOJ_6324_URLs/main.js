864649a593daa19c9fc96d4af34e509fa29eac40
LeeJaeYun <ag502@naver.com>
2021-01-22 12:36:01 +0900

    BOJ 6324

--- Problem/BOJ_6324_URLs/main.js
+++ Problem/BOJ_6324_URLs/main.js
@@ -1,29 +1,22 @@
 const fs = require('fs')
 
 function parsingURL(url) {
-    const answer = []
+    const urlRegex = /(http|ftp|gopher)\:\/\/([\w\.\-]+)(?:\:(\d+))?(?:\/([\w\W]+))?/
+    const matchedInfo = url.match(urlRegex)
 
-    const protocol = url.match(/^(\w+)\:\/\//)
-    answer.push(protocol[1])
-
-    const host = url.match(/^\w+\:\/\/([\w\-\.]+)(?:\:\d+)?(?:[\/\w\-\.\W]+)?$/)
-    console.log(host)
-    answer.push(host[1])
-
-    const port = url.match(/^\w+\:\/\/(?:[\w\-\.]+)(?:\:(\d+))?(?:[\/\w\-\.\W]+)?$/)
-    answer.push(!port[1] ? "<default>" : port[1])
-
-    const path = url.match(/^\w+\:\/\/(?:[\w\-\.]+)(?:\:\d+)?(?:\/([\/\w\-\.\W]+))?$/)
-    answer.push(!path[1] ? "<default>" : path[1])
-
-    return answer
+    return matchedInfo.slice(1, 5).map(parsedUrl => {
+        if (!parsedUrl) {
+            return "<default>"
+        }
+        return parsedUrl
+    })
 }
 
 function solution() {
     const input = fs.readFileSync("./input.txt")
         .toString()
+        .trim()
         .split("\n")
-        .map(elem => elem.trim())
 
     const numOfUrls = parseInt(input[0])
     input.slice(1).map((url, idx) => {
