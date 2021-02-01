582ef5cba3998ebb518521f9cc59ed0ea30baad0
LeeJaeYun <ag502@naver.com>
2021-02-01 21:05:48 +0900

    BOJ 5052

--- Problem/BOJ_5052_전화번호 목록/main.py
+++ Problem/BOJ_5052_전화번호 목록/main.py
@@ -1,31 +1,40 @@
 from sys import stdin
 
+
 def main():
+    stdin = open("./input.txt")
     test_case = int(stdin.readline())
+
     for _ in range(test_case):
-        is_consistence = True
-        num_of_phones = int(stdin.readline())
-        phones = [0] * num_of_phones
-        for idx in range(num_of_phones):
-            phones[idx] = stdin.readline().rstrip()
+        num_of_phone_number = int(stdin.readline())
 
-        phones.sort(key=lambda x: len(x))
+        # 전화번호 목록 리스트
+        phone_numbers = []
+        for _ in range(num_of_phone_number):
+            phone_numbers.append(stdin.readline().rstrip())
 
-        phones_set = set()
-        phones_set.add(phones[0])
+        # 길이 순 정렬
+        phone_numbers.sort(key=lambda x: len(x))
 
-        for phone_num in phones[1:]:
-            for idx in range(0, len(phone_num)):
-                if phone_num[:idx + 1] in phones_set:
+        front_part_of_number = set()
+
+        max_len = len(phone_numbers[0])
+        front_part_of_number.add(phone_numbers[0])
+
+        for phone_number in phone_numbers[1:]:
+            for num_of_char in range(1, max_len + 1):
+                if phone_number[:num_of_char] in front_part_of_number:
                     print("NO")
-                    is_consistence = False
                     break
-            if not is_consistence:
-                break
-
-            phones_set.add(phone_num)
-        if is_consistence:
+            else:
+                # 전화번호 접두어 저장
+                front_part_of_number.add(phone_number)
+                max_len = len(phone_number)
+                continue
+            break
+        else:
             print("YES")
+
 
 if __name__ == '__main__':
     main()
