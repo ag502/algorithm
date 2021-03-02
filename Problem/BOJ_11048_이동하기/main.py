6ed0ad275778b738aa9db82968a310aeac778dc8
LeeJaeYun <ag502@naver.com>
2021-03-02 15:20:08 +0900

    BOJ 11048

--- Problem/BOJ_11048_이동하기/main.py
+++ Problem/BOJ_11048_이동하기/main.py
@@ -1,15 +1,29 @@
 from sys import stdin
 
+moving_dir = [[0, 1], [1, 0], [1, 1]]
+
+
 def main():
-    n, m = map(int, stdin.readline().split())
-    candy_map = [list(map(int, stdin.readline().split())) for _ in range(n)]
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    stdin = open("./input.txt", "r")
+    rows, cols = map(int, stdin.readline().split())
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + candy_map[i - 1][j - 1]
+    maze = []
+    for _ in range(rows):
+        maze.append(list(map(int, stdin.readline().split())))
 
-    print(dp[n][m])
+    dp = [[0] * cols for _ in range(rows)]
+    dp[0][0] = maze[0][0]
 
-if __name__ == "__main__":
+    for row in range(rows):
+        for col in range(cols):
+            for moving_row, moving_col in moving_dir:
+                next_row = row + moving_row
+                next_col = col + moving_col
+                if 0 <= next_row < rows and 0 <= next_col < cols:
+                    dp[next_row][next_col] = max(dp[next_row][next_col], maze[next_row][next_col] + dp[row][col])
+
+    print(dp[rows - 1][cols - 1])
+
+
+if __name__ == '__main__':
     main()
