80e89f355bf9e137fca7472646aac93572740a97
LeeJaeYun <ag502@naver.com>
2021-03-04 18:38:07 +0900

    BOJ 5582 :white_check_mark: :fire: :fire:

---
+++ Problem/BOJ_5582_공통 부분 문자열/main.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+
+def main():
+    stdin = open("./input.txt", "r")
+    string1 = stdin.readline().rstrip()
+    string2 = stdin.readline().rstrip()
+
+    dp = [[0] * (len(string2) + 1) for _ in range(len(string1)+ 1)]
+
+    for string1_idx in range(1, len(string1) + 1):
+        for string2_idx in range(1, len(string2) + 1):
+            if string1[string1_idx - 1] == string2[string2_idx - 1]:
+                dp[string1_idx][string2_idx] = dp[string1_idx - 1][string2_idx - 1] + 1
+
+    answer = 0
+    for row in dp:
+        answer = max(answer, max(row))
+
+    print(answer)
+
+
+if __name__ == '__main__':
+    main()
