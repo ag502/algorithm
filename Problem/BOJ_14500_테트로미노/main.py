227eeedff4a64dbc05864976a28f5e48e4d51e93
LeeJaeYun <ag502@naver.com>
2021-03-07 16:40:50 +0900

    BOJ 14500 :white_check_mark:

--- Problem/BOJ_14500_테트로미노/main.py
+++ Problem/BOJ_14500_테트로미노/main.py
@@ -1,81 +1,56 @@
 from sys import stdin
 
+blocks = [
+    [[0, 0], [0, 1], [0, 2], [0, 3]], # '-' 모양
+    [[0, 0], [1, 0], [2, 0], [3, 0]],
+    [[0, 0], [0, 1], [1, 0], [1, 1]], # 'ㅁ' 모양
+    [[0, 0], [1, 0], [2, 0], [2, 1]], # 'L' 모양
+    [[0, 0], [0, 1], [0, 2], [1, 0]],
+    [[0, 0], [0, 1], [1, 1], [2, 1]],
+    [[1, 0], [1, 1], [1, 2], [0, 2]],
+    [[2, 0], [0, 1], [1, 1], [2, 1]],
+    [[0, 0], [1, 0], [1, 1], [1, 2]],
+    [[0, 0], [0, 1], [1, 0], [2, 0]],
+    [[0, 0], [0, 1], [0, 2], [1, 2]],
+    [[0, 0], [1, 0], [1, 1], [2, 1]], # 'Z' 모양
+    [[1, 0], [1, 1], [0, 1], [0, 2]],
+    [[1, 0], [2, 0], [1, 1], [0, 1]],
+    [[0, 0], [0, 1], [1, 1], [1, 2]],
+    [[0, 0], [0, 1], [0, 2], [1, 1]], # 'ㅗ' 모양
+    [[1, 0], [1, 1], [0, 1], [2, 1]],
+    [[0, 1], [1, 0], [1, 1], [1, 2]],
+    [[0, 0], [1, 0], [2, 0], [1, 1]]
+]
 
-def apply_tetromino():
-    sum_of_block = 0
-    # 가로 4칸
-    for row in range(rows):
-        for col in range(cols - 3):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row][col + 2] + paper[row][col + 3])
-    # 세로 4칸
-    for row in range(rows - 3):
-        for col in range(cols):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row + 2][col] + paper[row + 3][col])
-    
-    # 정사각형
-    for row in range(rows - 1):
-        for col in range(cols - 1):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row + 1][col] + paper[row + 1][col + 1])
-    
-    # 'L'자 (1), (5)
-    for row in range(rows - 2):
-        for col in range(cols - 1):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row + 2][col] + paper[row + 2][col + 1])
-            sum_of_block = max(sum_of_block, paper[row + 2][col] + paper[row][col + 1] + paper[row + 1][col + 1] + paper[row + 2][col + 1])
 
-    # 'L'자 (2), (6)
-    for row in range(rows - 1):
-        for col in range(cols - 2):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row][col + 1] + paper[row][col + 2])
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row + 1][col + 1] + paper[row + 1][col + 2])
-    # 'L'자 (3), (7)
-    for row in range(rows - 2):
-        for col in range(cols - 1):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row + 1][col + 1] + paper[row + 2][col + 1])
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row + 1][col] + paper[row + 2][col])
-    # 'L'자 (4), (8)
-    for row in range(rows - 1):
-        for col in range(cols - 2):
-            sum_of_block = max(sum_of_block, paper[row + 1][col] + paper[row + 1][col + 1] + paper[row + 1][col + 2] + paper[row][col + 2])
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row][col + 2] + paper[row + 1][col + 2])
-
-    # 'ㄴㄱ'자 (1), (3)
-    for row in range(rows - 2):
-        for col in range(cols - 1):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row + 1][col + 1] + paper[row + 2][col + 1])
-            sum_of_block = max(sum_of_block, paper[row][col + 1] + paper[row + 1][col + 1] + paper[row + 1][col] + paper[row + 2][col])
-    
-    # 'ㄴㄱ'자 (2), (4)
-    for row in range(rows - 1):
-        for col in range(cols - 2):
-            sum_of_block = max(sum_of_block, paper[row + 1][col] + paper[row + 1][col + 1] + paper[row][col + 1] + paper[row][col + 2])
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row + 1][col + 1] + paper[row + 1][col + 2])
-            
-    # 'ㅜ'자 (1), (3)
-    for row in range(rows - 1):
-        for col in range(cols - 2):
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row][col + 1] + paper[row][col + 2] + paper[row + 1][col + 1])
-            sum_of_block = max(sum_of_block, paper[row][col + 1] + paper[row + 1][col] + paper[row + 1][col + 1] + paper[row + 1][col + 2])
-
-    # 'ㅜ'자 (2), (4)
-    for row in range(rows - 2):
-        for col in range(cols - 1):
-            sum_of_block = max(sum_of_block, paper[row + 1][col] + paper[row][col + 1] + paper[row + 1][col + 1] + paper[row + 2][col + 1])
-            sum_of_block = max(sum_of_block, paper[row][col] + paper[row + 1][col] + paper[row + 2][col] + paper[row + 1][col + 1])
-            
-    print(sum_of_block)    
+def apply_tetromino(cur_row, cur_col):
+    sum_of_area = 0
+    for i in range(19):
+        temp = 0
+        for j in range(4):
+            next_row  = cur_row + blocks[i][j][0]
+            next_col = cur_col + blocks[i][j][1]
+            if (0 <= next_row < rows and 0 <= next_col < cols):
+                temp += paper[next_row][next_col]
+        sum_of_area = max(sum_of_area, temp)
+    return sum_of_area
 
 
 def main():
     stdin = open("Problem/BOJ_14500_테트로미노/input.txt", "r")
-    global rows, cols, paper
-    
+    global rows, cols, paper 
     rows, cols = map(int, stdin.readline().split())
-    paper = []
+    
+    paper =[]
     for _ in range(rows):
         paper.append(list(map(int, stdin.readline().split())))
     
-    apply_tetromino()
+    answer = 0
+    for row in range(rows):
+        for col in range(cols):
+            answer = max(apply_tetromino(row, col), answer)
+    
+    print(answer)
     
 
 if __name__ == "__main__":
