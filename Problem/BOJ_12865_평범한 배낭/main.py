07daf2a6ec43a91de14bd50ab28a75191f1658b6
LeeJaeYun <ag502@naver.com>
2021-03-26 03:51:01 +0900

    BOJ 12865

--- Problem/BOJ_12865_평범한 배낭/main.py
+++ Problem/BOJ_12865_평범한 배낭/main.py
@@ -1,6 +1,7 @@
 from sys import stdin
 
 def main():
+    stdin = open("Problem/BOJ_12865_평범한 배낭/input.txt", "r")
     n, k = list(map(int, stdin.readline().split()))
     cache = [[0] * (k + 1) for _ in range(0, n + 1)]
 
@@ -16,6 +17,9 @@ def main():
                 continue
             cache[i + 1][max_weight] = max(cache[i][max_weight], cache[i][max_weight - bag_objects[i][0]] + bag_objects[i][1])
     print(cache[n][k])
+    
+    for row in cache:
+        print(row)
 
 if __name__ == "__main__":
     main()
