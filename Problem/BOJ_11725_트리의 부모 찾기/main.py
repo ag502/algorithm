cd7bfe9023414d8f864d50fddeeaf3477ed0d7f7
LeeJaeYun <ag502@naver.com>
2021-04-23 22:41:19 +0900

    BOJ 11725

--- Problem/BOJ_11725_트리의 부모 찾기/main.py
+++ Problem/BOJ_11725_트리의 부모 찾기/main.py
@@ -1,41 +1,31 @@
 from sys import stdin, setrecursionlimit
-setrecursionlimit(10000)
+setrecursionlimit(1000000)
 
-def dfs(tree, visited, parents, cur_node, prev_node):
-    # 방문
+
+stdin = open("./input.txt", "r")
+num_of_node = int(stdin.readline())
+
+-0
+
+visited = [False] * (num_of_node + 1)
+parents = [0] * (num_of_node + 1)
+
+
+def dfs(cur_node):
     visited[cur_node] = True
-    if cur_node != 1:
-        parents[cur_node] = prev_node
 
-    # 주변 탐색
-    for next_node in tree[cur_node]:
+    for next_node in trees[cur_node]:
         if not visited[next_node]:
-            dfs(tree, visited, parents, next_node, cur_node)
-
-    # 체크아웃
-    visited[cur_node] = False
+            parents[next_node] = cur_node
+            dfs(next_node)
 
 
 def main():
-    stdin = open('./input.txt', 'r')
-    num_of_nodes = int(stdin.readline())
+    dfs(1)
 
-    tree = {}
-    for node in range(1, num_of_nodes + 1):
-        tree[node] = []
+    for parent in parents[2:]:
+        print(parent)
 
-    for _ in range(num_of_nodes - 1):
-        node_1, node_2 = map(int, stdin.readline().split())
-        tree[node_1].append(node_2)
-        tree[node_2].append(node_1)
-
-    visited = [False] * (num_of_nodes + 1)
-    parent = [-1] * (num_of_nodes + 1)
-
-    dfs(tree, visited, parent, 1, None)
-
-    for parent_node in parent[2:]:
-        print(parent_node)
 
 if __name__ == '__main__':
     main()
