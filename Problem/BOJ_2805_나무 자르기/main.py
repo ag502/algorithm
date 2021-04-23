9ccefff8b77d0dae4b0795925acdb7e441705941
LeeJaeYun <ag502@naver.com>
2021-04-23 15:26:54 +0900

    BOJ 2805 :white_check_mark:

--- Problem/BOJ_2805_나무 자르기/main.py
+++ Problem/BOJ_2805_나무 자르기/main.py
@@ -1,30 +1,63 @@
 from sys import stdin
 
 
+# def main():
+#     n, m = map(int, stdin.readline().split())
+#     trees = sorted(list(map(int, stdin.readline().split())))
+#     # heights = [i for i in range(max(trees) + 1)]
+#
+#     start = 0
+#     end = max(trees)
+#     candidate_height = []
+#     while start <= end:
+#         mid = (start + end) // 2
+#         sum_of_cut = sum([tree - mid if tree -
+#                           mid > 0 else 0 for tree in trees])
+#
+#         if sum_of_cut == m:
+#             print(mid)
+#             return
+#         elif sum_of_cut > m:
+#             start = mid + 1
+#             candidate_height.append(mid)
+#         elif sum_of_cut < m:
+#             end = mid - 1
+#
+#     print(max(candidate_height))
+#
+#
+# if __name__ == "__main__":
+#     main()
+
+
+def get_tree(trees, cur_height):
+    height_sum = 0
+
+    for tree in trees:
+        if tree > cur_height:
+            height_sum += tree - cur_height
+
+    return height_sum
+
+
 def main():
-    n, m = map(int, stdin.readline().split())
-    trees = sorted(list(map(int, stdin.readline().split())))
-    # heights = [i for i in range(max(trees) + 1)]
+    stdin = open("./input.txt", "r")
+    num_of_tree, target_height = map(int, stdin.readline().split())
+    tree_height = list(map(int, stdin.readline().split()))
 
     start = 0
-    end = max(trees)
-    candidate_height = []
+    end = 1000000000
+
     while start <= end:
         mid = (start + end) // 2
-        sum_of_cut = sum([tree - mid if tree -
-                          mid > 0 else 0 for tree in trees])
+        get_tree_height = get_tree(tree_height, mid)
 
-        if sum_of_cut == m:
-            print(mid)
-            return
-        elif sum_of_cut > m:
-            start = mid + 1
-            candidate_height.append(mid)
-        elif sum_of_cut < m:
+        if get_tree_height <= target_height:
             end = mid - 1
+        else:
+            start = mid + 1
+    print(start)
 
-    print(max(candidate_height))
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
