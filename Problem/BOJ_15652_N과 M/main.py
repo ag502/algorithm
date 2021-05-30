7efee7f41f61d014bf71a537af889d676ad7a231
LeeJaeYun <ag502@naver.com>
2021-05-30 19:10:48 +0900

    BOJ 15652 :white_check_mark:

--- Problem/BOJ_15652_N과 M/main.py
+++ Problem/BOJ_15652_N과 M/main.py
@@ -1,30 +1,27 @@
 from sys import stdin
+from typing import List
 
 
 def main():
-    N, M = map(int, stdin.readline().split())
+    stdin = open("./input.txt", "r")
 
-    for i in range(1, N + 1):
-        m_combination(i, N, M, 0, [])
+    n: int
+    m: int
+    n, m = list(map(int, stdin.readline().split()))
 
+    def find_sequence(cur_num: int, sequence: List[int] = []):
+        sequence.append(cur_num)
 
-def m_combination(cur_num, n, m, selected_num, answer):
-    # 1. 체크인
-    selected_num += 1
-    # 2. 목적지
-    answer.append(str(cur_num))
-    # 3. 인접 노드 순회
-    for next_num in range(cur_num, n + 1):
-        # 4. 갈 수 있는지 검사
-        if selected_num != m:
-            # 5. 간다
-            m_combination(next_num, n, m, selected_num, answer)
-    # 6. 더 이상 갈 곳이 없을 경우 체크 아웃
-    if len(answer) == m:
-        print(' '.join(answer))
-    answer.pop()
-    selected_num -= 1
+        if len(sequence) < m:
+            for next_num in range(cur_num, n + 1):
+                find_sequence(next_num, sequence)
+        elif len(sequence) == m:
+            print(' '.join(map(str, sequence)))
+        sequence.pop()
+
+    for start_num in range(1, n + 1):
+        find_sequence(start_num)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
