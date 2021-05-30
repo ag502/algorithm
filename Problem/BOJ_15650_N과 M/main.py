4bbf4364acb64ccc124a4a269b0a88d034d48d50
LeeJaeYun <ag502@naver.com>
2021-05-30 21:01:52 +0900

    BOJ 15650 :white_check_mark:

--- Problem/BOJ_15650_N과 M/main.py
+++ Problem/BOJ_15650_N과 M/main.py
@@ -1,31 +1,30 @@
 from sys import stdin
+from typing import List
 
 
 def main():
-    N, M = map(int, stdin.readline().split())
-    visited = [False] * (N + 1)
-    for i in range(1, N + 1):
-        combination(i, N, M, 0, visited, [])
+    stdin = open("./input.txt", "r")
+    n: int
+    m: int
+    n, m = list(map(int, stdin.readline().split()))
+    visited: List[int] = [False] * (n + 1)
+
+    def find_sequence(cur_num: int, sequence: List[int] = []):
+        sequence.append(cur_num)
+        visited[cur_num] = True
+
+        if len(sequence) < m:
+            for next_num in range(cur_num, n + 1):
+                if not visited[next_num]:
+                    find_sequence(next_num, sequence)
+        elif len(sequence) == m:
+            print(' '.join(map(str, sequence)))
+        sequence.pop()
+        visited[cur_num] = False
+
+    for start_num in range(1, n + 1):
+        find_sequence(start_num)
 
 
-def combination(cur_num, n, m, selected_num, visited, answer):
-    # 1. 체크인
-    visited[cur_num] = True
-    selected_num += 1
-    # 2. 도착
-    answer.append(str(cur_num))
-    # 3. 인접 노드 순회
-    for next_num in range(cur_num + 1, n + 1):
-        # 4. 갈 수 있는지 검사
-        if not visited[next_num] and selected_num != m:
-            combination(next_num, n, m, selected_num, visited, answer)
-    # 5. 체크아웃
-    if len(answer) == m:
-        print(' '.join(answer))
-    answer.pop()
-    visited[cur_num] = False
-    selected_num -= 1
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
