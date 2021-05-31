b3e93695c742133079ef735ac7a8e457e4e81ffc
LeeJaeYun <ag502@naver.com>
2021-05-31 21:19:19 +0900

    BOJ 16937 convert to class :white_check_mark:

--- Problem/BOJ_16937_두 스티커/main.py
+++ Problem/BOJ_16937_두 스티커/main.py
@@ -1,73 +1,64 @@
 from sys import stdin
-from typing import List, Tuple
 
 
-def input() -> Tuple[int, int, int, List[List[int]]]:
-    stdin = open("./input.txt", "r")
+class Main:
+    def __init__(self):
+        self.height = 0
+        self.width = 0
+        self.num_of_stickers = 0
+        self.stickers = []
+        self.visited = None
+        self.selected_sticker = None
+        self.max_area = 0
 
-    height: int
-    width: int
-    height, width = list(map(int, stdin.readline().split()))
+        self.main()
 
-    num_of_sticker: int
-    num_of_sticker = int(stdin.readline())
+    def main(self):
+        stdin = open("./input.txt", "r")
+        self.height, self.width = map(int, stdin.readline().split())
+        self.num_of_stickers = int(stdin.readline())
 
-    stickers: List[List[int]] = []
+        for sticker in range(self.num_of_stickers):
+            sticker_1, sticker_2 = map(int, stdin.readline().split())
+            self.stickers.append([sticker_1, sticker_2, sticker])
 
-    sticker_1: int
-    sticker_2: int
-    for sticker in range(num_of_sticker):
-        sticker_1, sticker_2 = list(map(int, stdin.readline().split()))
+            if sticker_1 != sticker_2:
+                self.stickers.append([sticker_2, sticker_1, sticker])
 
-        stickers.append([sticker_1, sticker_2, sticker])
-        if sticker_1 != sticker_2:
-            stickers.append([sticker_2, sticker_1, sticker])
+        self.visited = [False] * len(self.stickers)
+        self.selected_sticker = [False] * self.num_of_stickers
 
-    return height, width, num_of_sticker, stickers
+        for start_sticker in range(len(self.stickers)):
+            self.pick_sticker(start_sticker)
 
-sum_of_area = 0
+        print(self.max_area)
 
+    def pick_sticker(self, cur_sticker, two_stickers=[]):
+        cur_sticker_h, cur_sticker_w, cur_sticker_no = self.stickers[cur_sticker]
+        two_stickers.append(self.stickers[cur_sticker])
+        self.visited[cur_sticker] = True
+        self.selected_sticker[cur_sticker_no] = True
 
-def main() -> None:
-    global sum_of_area
+        if len(two_stickers) < 2:
+            for next_sticker in range(cur_sticker + 1, len(self.stickers)):
+                next_sticker_h, next_sticker_w, next_sticker_no = self.stickers[next_sticker]
+                if not self.visited[next_sticker] and not self.selected_sticker[next_sticker_no]:
+                    self.pick_sticker(next_sticker, two_stickers)
+        elif len(two_stickers) == 2:
+            self.check_attach(two_stickers[0], two_stickers[1])
+        two_stickers.pop()
+        self.visited[cur_sticker] = False
+        self.selected_sticker[cur_sticker_no] = False
 
-    height, width, num_of_sticker, stickers = input()
+    def check_attach(self, sticker_1, sticker_2):
+        height_1, width_1, _ = sticker_1
+        height_2, width_2, _ = sticker_2
 
-    visited: List[bool] = [False] * len(stickers)
-    selected_sticker: List[bool] = [False] * num_of_sticker
-
-    def select_sticker(cur_sticker: int, sticker: List[List[int]] = []):
-        global sum_of_area
-
-        cur_sticker_num = stickers[cur_sticker][2]
-        sticker.append(stickers[cur_sticker])
-        visited[cur_sticker] = True
-        selected_sticker[cur_sticker_num] = True
-
-        if len(sticker) < 2:
-            for next_sticker in range(cur_sticker, len(stickers)):
-                if not visited[next_sticker] and not selected_sticker[stickers[next_sticker][2]]:
-                    select_sticker(next_sticker, sticker)
-        elif len(sticker) == 2:
-            height_1, width_1 = [sticker[0][0], sticker[0][1]]
-            height_2, width_2 = [sticker[1][0], sticker[1][1]]
-
-            if height_1 <= height and height_2 <= height and width_1 <= width and width_2 <= width:
-                if height_1 + height_2 <= height:
-                    sum_of_area = max(sum_of_area, height_1 * width_1 + height_2 * width_2)
-
-                elif width_1 + width_2 <= width:
-                    sum_of_area = max(sum_of_area, height_1 * width_1 + height_2 * width_2)
-
-        sticker.pop()
-        visited[cur_sticker] = False
-        selected_sticker[stickers[cur_sticker][2]] = False
-
-    for start_sticker in range(len(stickers)):
-        select_sticker(start_sticker)
-
-    print(sum_of_area)
+        if height_1 <= self.height and height_2 <= self.height and \
+                width_1 <= self.width and width_2 <= self.width:
+            if height_1 + height_2 <= self.height or width_1 + width_2 <= self.width:
+                self.max_area = max(self.max_area, height_1 * width_1 + height_2 * width_2)
 
 
 if __name__ == '__main__':
-    main()
+    Main()
