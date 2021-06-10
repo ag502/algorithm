74dcd7cde92f8065e1af1f2f25f915e13b4b24cc
LeeJaeYun <ag502@naver.com>
2021-06-10 23:32:41 +0900

    BOJ 2295 :white_check_mark: :fire:

--- Problem/BOJ_2295_세 수의 합/main.py
+++ Problem/BOJ_2295_세 수의 합/main.py
@@ -1,38 +1,69 @@
 from sys import stdin
+from collections import deque
 
 
-def binary_search(array, start_idx, target):
-    start = start_idx
-    end = len(array) - 1
+class Main:
+    def __init__(self):
+        self.num_of_elem = None
+        self.set = None
+        self.sum_1 = deque()
+        self.sum_2 = deque()
 
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] > target:
-            end = mid - 1
-        elif array[mid] < target:
-            start = mid + 1
-        else:
-            return mid
-    return -1
+        self.main()
 
+    def lower_bound(self, target):
+        left_ptr = 0
+        right_ptr = len(self.sum_2)
 
-def main():
-    stdin = open('input.txt', 'r')
-    num_of_set = int(stdin.readline())
+        while left_ptr < right_ptr:
+            mid = (left_ptr + right_ptr) // 2
+            if self.sum_2[mid][1] < target:
+                left_ptr = mid + 1
+            elif self.sum_2[mid][1] >= target:
+                right_ptr = mid
+        return right_ptr
 
-    number_set = [0] * num_of_set
-    for idx in range(num_of_set):
-        number_set[idx] = int(stdin.readline())
+    def upper_bound(self, target):
+        left_ptr = 0
+        right_ptr = len(self.sum_2)
 
-    number_set.sort()
-    for target_idx in range(len(number_set) - 1, -1, -1):
-        target_number = number_set[target_idx]
-        for i in range(len(number_set)):
-            for j in range(i, len(number_set)):
-                if binary_search(number_set, j, target_number - number_set[i] - number_set[j]) != -1:
-                    print(target_number)
-                    return
+        while left_ptr < right_ptr:
+            mid = (left_ptr + right_ptr) // 2
+            if self.sum_2[mid][1] <= target:
+                left_ptr = mid + 1
+            elif self.sum_2[mid][1] > target:
+                right_ptr = mid
+        return right_ptr
+
+    def main(self):
+        stdin = open("./input.txt", "r")
+        self.num_of_elem = int(stdin.readline())
+        self.set = [int(stdin.readline()) for _ in range(self.num_of_elem)]
+        self.set.sort()
+
+        for x in range(self.num_of_elem):
+            for y in range(x, self.num_of_elem):
+                self.sum_1.append(self.set[x] + self.set[y])
+
+        for k in self.set:
+            for z in self.set:
+                if k < z:
+                    break
+                self.sum_2.append([k, k - z])
+
+        self.sum_2 = list(self.sum_2)
+        self.sum_2.sort(key=lambda x: x[1])
+
+        answer = 0
+        for sub_sum in self.sum_1:
+            lower_idx = self.lower_bound(sub_sum)
+            upper_idx = self.upper_bound(sub_sum)
+
+            if lower_idx != upper_idx:
+                answer = max(answer, self.sum_2[upper_idx - 1][0])
+
+        print(answer)
 
 
 if __name__ == '__main__':
-    main()
+    Main()
